b7ebf387c2f30003d86e2a34087382e3cba757b3
Alexey Loik <samloik@mail.ru>
2023-03-14 17:01:51 +1000

    обновление фунция test2 измеяет количетсво возможных запросов

--- parse_page_domik.py
+++ parse_page_domik.py
@@ -8,10 +8,25 @@ def test():
 
     contacts = ['709088', '584930', '420560', '713567', '199781', '717797']
 
+
     for contact in contacts:
         print(f'Контакт: [{contact}] тел: {get_phone_number(contact)}')
 
 
+import random
+from time_decorator import *
+@timeit
+def test2():
+
+    contacts = ['709088', '584930', '420560', '713567', '199781', '717797']
+
+    i=0
+    while True:
+        contact = random.choice(contacts)
+        print(f'[{i:5}] Контакт: [{contact}] тел: {get_phone_number(contact)}')
+        i += 1
+
+
 if __name__ == '__main__':
-    test()
+    test2()
 
