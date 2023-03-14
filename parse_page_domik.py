f53acfc72aeeef84e655d45cfc606e07340d24fe
Alexey Loik <samloik@mail.ru>
2023-03-14 15:36:25 +1000

    обновление

--- parse_page_domik.py
+++ parse_page_domik.py
@@ -9,8 +9,9 @@ def test():
     contacts = ['709088', '584930', '420560', '713567', '199781', '717797']
 
     for contact in contacts:
-        print(f'Контакт: [{contact}] тел: [{get_phone_number(contact)}]')
+        print(f'Контакт: [{contact}] тел: {get_phone_number(contact)}')
 
 
 if __name__ == '__main__':
     test()
+
